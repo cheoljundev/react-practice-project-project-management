6fdc5cc9a3861535002725b38cb21d3869859dff
cheoljundev <cheoljundev@gmail.com>
2024-07-14 21:37:00 +0900

    태스크 지우기 & 사소한 버그 고치기

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,16 @@ function App() {
         })
     }
 
-    function handleDeleteTask() {}
+    function handleDeleteTask(id) {
+        setProjectState(prevState => {
+            return {
+                ...prevState,
+                tasks: prevState.tasks.filter(
+                    task => task.id !== id
+                ),
+            }
+        })
+    }
 
     function handleSelectProject(id) {
         setProjectState(prevState => {
@@ -108,6 +117,7 @@ function App() {
                 onStartAddProject={handleStartAddProject}
                 projects={projectState.projects}
                 onSelectProject={handleSelectProject}
+                selectedProjectId={projectState.selectedProjectId}
             />
             {content}
         </main>

8f017d4e10b3802f0e99947fa6f67218ac59dc7b
cheoljundev <cheoljundev@gmail.com>
2024-07-14 17:53:59 +0900

    선택 가능한 프로젝트 구현 및 프로젝트 정보 보기

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import {useState} from "react";
+import SelectedProject from "./components/SelectedProject.jsx";
 
 function App() {
     const [projectState, setProjectState] = useState({
         selectedProjectId: undefined,
         projects: []
     })
+
+    function handleSelectProject(id) {
+        setProjectState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectId: id,
+            }
+        })
+    }
 
     function handleStartAddProject() {
         setProjectState(prevState => {
@@ -43,7 +53,9 @@ function App() {
         })
     }
 
-    let content;
+    const selectedProject = projectState.projects.find(project => project.id === projectState.selectedProjectId);
+
+    let content = <SelectedProject project={selectedProject}/>;
 
     if (projectState.selectedProjectId === null) {
         content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>;
@@ -56,6 +68,7 @@ function App() {
             <ProjectsSidebar
                 onStartAddProject={handleStartAddProject}
                 projects={projectState.projects}
+                onSelectProject={handleSelectProject}
             />
             {content}
         </main>

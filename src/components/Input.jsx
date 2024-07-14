220988cbdfd90b588cca581c86cc0441cb8977c7
cheoljundev <cheoljundev@gmail.com>
2024-07-14 16:05:59 +0900

    테일윈드 CSS로 버튼과 입력 항목 스타일링

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,15 @@
 export default function Input({label, textarea, ...props}) {
-    return <p>
-        <label>{label}</label>
-        {textarea ? <textarea {...props}/> : <input type="text"/>}
-    </p>
+
+    const classes = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
+
+    return (
+        <p className="flex flex-col gap-1 my-4 font-bold uppercase text-stone-500">
+            <label>{label}</label>
+            {textarea ? (
+                <textarea className={classes} {...props}/>
+            ) : (
+                <input className={classes} type="text"/>
+            )}
+        </p>
+    );
 };
